4dd99247ac83e060a1e1bc6ccc4cf52f7e4a2426
Gustavo <gustavojesus79@gmail.com>
2025-08-08 18:45:35 -0300

    mudando titulo

--- todolist-frontend/src/TarefasCrud.jsx
+++ todolist-frontend/src/TarefasCrud.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 
-
 const TarefasCrud = () => {
   const [tarefas, setTarefas] = useState([]);
   const [loading, setLoading] = useState(true);
