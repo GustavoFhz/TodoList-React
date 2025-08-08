4ae4ce5bc8f255a38a89dcd18bcd8eed4cb11589
Gustavo <gustavojesus79@gmail.com>
2025-08-08 18:01:08 -0300

    correcao

--- todolist-backend/TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ todolist-backend/TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21389f5816952579c5d693889bf8962f8006946")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
